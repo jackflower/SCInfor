0a46a83514ad96183d7f0578f4414ee2df209e07
jackflower <jackflower@poczta.onet.pl>
2019-02-13 16:55:50 +0100

    Update - refaktoryzacja kodu. Dodanie całej nowej klasy reprezentującej opwakowanie sf::Texture. Porządki w kodzie. Dość czasochłonna operacja - zysk możliwość korzystania z różnych framework.

---
+++ SCInfor/source/Rendering/Animated/AnimatedAnimation.cpp
@@ -0,0 +1,146 @@
+﻿//  ______________________________________________
+// | AnimatedAnimation.cpp - class implementation |
+// | Jack Flower - March 2009                     |
+// |______________________________________________|
+//
+
+#include "AnimatedAnimation.h"
+
+namespace animated
+{
+
+	RTTI_IMPL_NOPARENT(AnimatedAnimation);
+
+	//Konstruktor
+	AnimatedAnimation::AnimatedAnimation()
+	{
+	}
+
+	//Konstruktor kopiujący
+	AnimatedAnimation::AnimatedAnimation(const AnimatedAnimation& copy)
+		:
+		m_frame(copy.m_frame)
+	{
+	}
+
+	//Konstruktor parametryczny
+	AnimatedAnimation::AnimatedAnimation(const rendering::displayable::Texture & texture, int width_frame, int height_frame)
+	{
+		setAnimationFramesFromAtlasTexture(texture, width_frame, height_frame);
+	}
+
+	//Konstruktor przenoszący
+	AnimatedAnimation::AnimatedAnimation(AnimatedAnimation && other)
+	:
+		m_frame(other.m_frame)
+	{
+	}
+
+	//Destruktor
+	AnimatedAnimation::~AnimatedAnimation()
+	{
+		m_frame.clear();
+	}
+
+	//Przeciążony operator przypisania kopiowania
+	AnimatedAnimation & AnimatedAnimation::operator=(const AnimatedAnimation & copy)
+	{
+		if (this != &copy)
+			m_frame = copy.m_frame;
+		return *this;
+	}
+
+	//Przeciążony operator przypisania przenoszenia
+	AnimatedAnimation & AnimatedAnimation::operator=(AnimatedAnimation && other)
+	{
+		if (this != &other)
+		{
+			m_frame = other.m_frame;
+
+			//zerowanie obiektu źródłowego
+			other.m_frame.clear();
+		}
+		return *this;
+	}
+
+	//Definicja operatora dla obiektu klasy AnimatedFrame
+	AnimatedFrame & AnimatedAnimation::operator[] (unsigned N)
+	{
+		return m_frame[N];
+	}
+
+	//Metoda zwraca typ obiektu /RTTI/
+	const std::string AnimatedAnimation::getType() const
+	{
+		return rtti.GetNameClass();
+	}
+
+	//Metoda zwraca ilość obieków klasy AnimatedFrame w wektorze
+	unsigned int AnimatedAnimation::size() const
+	{
+		return m_frame.size();
+	}
+
+	//Metoda dodaje do wektora obiekty klasy AnimatedFrame
+	bool AnimatedAnimation::setAnimationFramesFromAtlasTexture(const Texture & texture, int width_frame, int height_frame)
+	{
+		//Zakładamy, że szerokość i wysokość tekstury jest dodatnia
+		if (width_frame <= 0 || height_frame <= 0)
+			return false;//rozmiar musi być dodatni, a dzielnik nie może być równy zero...
+		else
+		{
+			int width_i = texture.getSize().x / width_frame;	//pobieramy szerokość tekstury
+			int height_j = texture.getSize().y / height_frame;	//pobieramy wysokość tekstury
+
+			for (int j = 0 ; j < height_j; j++)
+			{
+				for (int i = 0 ; i  <width_i; i++)
+				{
+					pushFrame(AnimatedFrame(texture, sf::IntRect
+								(	width_frame*i,
+									height_frame*j,
+									width_frame,
+									height_frame
+								)));
+				}
+			}
+			return true;
+		}
+	}
+
+	//Metoda dodaje do wektora obiekt klasy CAnimFrame
+	void AnimatedAnimation::setSingleAnimationFrameTexture(const Texture & texture, const sf::IntRect & rectangle)
+	{
+		pushFrame(AnimatedFrame(texture, rectangle));
+	}
+
+	//Metoda usuwa z wektora obiekt klasy CAnimFrame o podanym indeksie
+	void AnimatedAnimation::deleteAnimFrame(int index)
+	{
+		m_frame.erase(m_frame.begin() + index);
+	}
+
+	//Metoda usuwa z wektora wszystkie elementy
+	void AnimatedAnimation::clearAllAnimFrame()
+	{
+		m_frame.clear();
+	}
+
+	//Metoda zwraca z wektora szerokość klatki animacji o podanym indeksie
+	int const AnimatedAnimation::getAnimFrameWidth(int animframe_index)
+	{
+		return m_frame[animframe_index].getFrameRectangle().width;
+	}
+
+	//Metoda zwraca z wektora wysokość klatki animacji o podanym indeksie
+	int const AnimatedAnimation::getAnimFrameHeight(int animframe_index)
+	{
+		return m_frame[animframe_index].getFrameRectangle().height;
+	}
+
+	//Metoda wstawia do wektora pojedynczy obiekt klasy AnimatedFrame
+	void AnimatedAnimation::pushFrame(const AnimatedFrame & new_frame)
+	{
+		m_frame.push_back(new_frame);
+	}
+}//namespace animated
---
+++ SCInfor/source/Rendering/Animated/AnimatedAnimation.h
@@ -0,0 +1,145 @@
+﻿//  ______________________________________________
+// | AnimatedAnimation.h - class definition       |
+// | Jack Flower - March 2009                     |
+// |______________________________________________|
+//
+
+#ifndef H_ANIMATED_ANIMATION_JACK
+#define H_ANIMATED_ANIMATION_JACK
+
+#include "AnimatedFrame.h"
+#include "../../RTTI/RTTI.h"
+#include <vector>
+
+
+namespace animated
+{
+
+	///
+	///Klasa reprezentuje animację
+	///
+	class AnimatedAnimation
+	{
+		RTTI_DECL;
+
+	public:
+
+		///
+		///Konstruktor domyślny
+		///
+		AnimatedAnimation();
+
+		///
+		///Konstruktor kopiujący
+		///
+		///@param copy - stała referencja na obiekt klasy AnimatedAnimation
+		///
+		AnimatedAnimation(const AnimatedAnimation & copy);
+
+		///
+		///Konstruktor przenoszący
+		///
+		///@param other = referencja do r-wartości
+		///
+		AnimatedAnimation(AnimatedAnimation && other);
+
+		///
+		///Konstruktor parametryczny
+		///
+		///@param texture - tekstura
+		///
+		///@param width_frame - szerokość klatki animacji
+		///
+		///@param height_frame - wysokość klatki animacji
+		///
+		AnimatedAnimation(const rendering::displayable::Texture & texture, int width_frame, int height_frame);
+		
+		///
+		///Destruktor
+		///
+		virtual ~AnimatedAnimation();
+
+		///
+		///Przeciążony operator przypisania kopiowania
+		///
+		///@param copy - stała referencja na obiekt klasy Animation
+		///
+		AnimatedAnimation & operator=(const AnimatedAnimation & copy);
+
+		///
+		///Przeciążony operator przypisania przenoszenia
+		///
+		///@param other -  referencja do r-wartości
+		///
+		AnimatedAnimation & operator=(AnimatedAnimation && other);
+
+		///
+		///Deklaracja operatora dla obiektu klasy AnimatedFrame
+		///
+		AnimatedFrame & operator[] (unsigned N);
+
+		///
+		///Metoda zwraca typ obiektu /RTTI/
+		///
+		const std::string getType() const;
+
+		///
+		///Metoda zwraca ilość obiektów klasy AnimatedFrame w wektorze
+		///
+		unsigned int size() const;
+
+		///
+		///Metoda dodaje do wektora obiekty klasy AnimatedFrame
+		///
+		///@param texture - tekstura
+		///
+		///@param width_frame - szerokość klatki animacji
+		///
+		///@param height_frame - wysokość klatki animacji
+		///
+		bool setAnimationFramesFromAtlasTexture(const Texture & texture, int width_frame, int height_frame);
+
+		///
+		///Metoda dodaje do wektora obiekt klasy AnimatedFrame
+		///
+		///@param texture - tekstura
+		///
+		///@param rectangle - obszar prostokątny
+		///
+		void setSingleAnimationFrameTexture(const Texture & texture, const sf::IntRect & rectangle );
+
+		///
+		///Metoda usuwa z wektora obiekt klasy CAnimFrame o podanym indeksie
+		///
+		///@param index - indeks klatki do usunięcia
+		///
+		void deleteAnimFrame(int index);
+
+		///
+		///Metoda usuwa z wektora wszystkie elementy
+		///
+		void clearAllAnimFrame();
+	
+		///
+		///Metoda zwraca z wektora szerokość klatki animacji o podanym indeksie
+		///
+		///@param animframe_index - indeks klatki animacji
+		///
+		int const getAnimFrameWidth(int animframe_index);
+	
+		///
+		///Metoda zwraca z wektora wysokość klatki animacji o podanym indeksie
+		///
+		///@param animframe_index - indeks klatki animacji
+		///
+		int const getAnimFrameHeight(int animframe_index);
+
+	private:
+
+		std::vector<AnimatedFrame> m_frame; //kontener na klatki animacji
+
+		//metoda wstawia do wektora pojedynczy obiekt klasy AnimatedFrame
+		void pushFrame(const AnimatedFrame & new_frame);
+	};
+}//namespace animated
+#endif //H_ANIMATED_ANIMATION_JACK
