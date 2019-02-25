771f4bdc99e20b202e6f0e6204f0ec8f2c3bf506
jackflower <jackflower@poczta.onet.pl>
2019-02-25 09:34:32 +0100

    Update - refaktoryzacja kody. Testy zapisy standardu C++11.

---
+++ SCInfor/source/Rendering/Particle/Particle.h
@@ -0,0 +1,85 @@
+﻿//  _____________________________________
+// | Particle.h - class definition       |
+// | Jack Flower - May 2016              |
+// |_____________________________________|
+//
+
+#ifndef H_PARTICLE_JACK
+#define H_PARTICLE_JACK
+
+#include "ParticleState.h"
+#include "../../RTTI/RTTI.h"
+
+namespace particle
+{
+	///
+	///Klasa reprezentuje cząsteczkę
+	///
+	class Particle
+	{
+		RTTI_DECL;
+
+	public:
+
+		///
+		///Konstruktor
+		///
+		Particle();
+
+		///
+		///Konstruktor kopiujący
+		///
+		///@param copy - stała referencja na obiekt klasy Particle
+		///
+		Particle(const Particle & copy);
+
+		///
+		///Konstruktor przenoszący
+		///
+		///@param other - referencja do r-wartości
+		///
+		Particle(Particle && other);
+
+		///
+		///Destruktor
+		///
+		~Particle();
+
+		///
+		///Metoda zwraca typ obiektu /RTTI/
+		///
+		const std::string getType() const;
+
+		///
+		///Przeciążony operator przypisania kopiowania
+		///
+		///@param copy - stała referencja na obiekt klasy Particle
+		///
+		Particle & operator=(const Particle & copy);
+
+		///
+		///Przeciążony operator przypisania przenoszenia
+		///
+		///@param other -  referencja do r-wartości
+		///
+		Particle & operator=(Particle && other);
+
+		
+		ParticleState m_current; //aktualne parametry cząsteczki
+		ParticleState m_change; //docelowe parametry cząsteczki 
+		sf::Vector2f m_position; //pozycja cząsteczki
+		sf::Vector2f m_velocity; //prędkość cząsteczki (składowe x i y)
+		float m_change_duration; //czas w jakim cząsteczka ma przejść w stan docelowy
+
+		///
+		///Metoda aktualizuje parametry cząsteczki zanim cząsteczka przejdzie w stan docelowy
+		///
+		void CalculateChange();
+
+	private:
+
+		Particle *p_next_particle;	//wskaźnik ma następną cząsteczkę
+		Particle *p_prev_particle;	//wskaźnik ma poprzednią cząsteczkę
+	};
+}//namespace particle
+#endif//H_PARTICLE_JACK
---
+++ SCInfor/source/Rendering/Particle/ParticleState.cpp
@@ -0,0 +1,89 @@
+﻿//  __________________________________________
+// | ParticleState.cpp - class implementation |
+// | Jack Flower - May 2016                   |
+// |__________________________________________|
+//
+
+#include "ParticleState.h"
+
+namespace particle
+{
+	RTTI_IMPL_NOPARENT(ParticleState);
+
+	//Konstruktor
+	ParticleState::ParticleState()
+	:
+		m_color{},
+		m_angle{ 0.0f },
+		m_size{ 0.0f, 0.0f }
+	{
+	}
+
+	//Konstruktor kopiujący
+	ParticleState::ParticleState(const ParticleState & copy)
+	:
+		m_color{ copy.m_color },
+		m_angle{ copy.m_angle },
+		m_size{ copy.m_size }
+	{
+	}
+
+	//Konstruktor przenoszący
+	ParticleState::ParticleState(ParticleState && other)
+	:
+		m_color{ other.m_color }, //konstruktor przenoszący klasy bazowej
+		m_angle{ other.m_angle },
+		m_size{ other.m_size }
+	{
+		//zerujemy składowe obiektu źródłowego...
+		other.m_color;
+		other.m_angle = 0.f;
+		other.m_size.x = 0.f;
+		other.m_size.y = 0.f;
+	}
+
+	//Destruktor
+	ParticleState::~ParticleState()
+	{
+		m_color;
+		m_angle = 0.0f;
+		m_size.x = 0.0f;
+		m_size.y = 0.0f;
+	}
+
+	//Metoda zwraca typ obiektu /RTTI/
+	const std::string ParticleState::getType() const
+	{
+		return rtti.GetNameClass();
+	}
+
+	//Przeciążony operator przypisania kopiowania
+	ParticleState & ParticleState::operator=(const ParticleState & copy)
+	{
+		if (this != &copy)
+		{
+			m_color = copy.m_color;
+			m_angle = copy.m_angle;
+			m_size = copy.m_size;
+		}
+		return *this;
+	}
+
+	//Przeciążony operator przypisania przenoszenia
+	ParticleState & ParticleState::operator=(ParticleState && other)
+	{
+		if (this != &other)
+		{
+			m_color = other.m_color;
+			m_angle = other.m_angle;
+			m_size = other.m_size;
+
+			//zerowanie obiektu źródłowego
+			other.m_color;
+			other.m_angle = 0.0f;
+			other.m_size.x = 0.0f;
+			other.m_size.y = 0.0f;
+		}
+		return *this;
+	}
+}//namespace particle
