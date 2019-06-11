016573d4153fd992598c14f5ca8ffbb933707a17
jackflower <jackflower@poczta.onet.pl>
2019-06-11 14:56:39 +0200

    Dodanie klasy opalpwującej std:vector - na podstawie opracowania dla standardu C++17 - nowy silnik kontenerów na obiekty. Początek testów.

---
+++ SCInfor/source/Containers/Vector.cpp
@@ -0,0 +1,21 @@
+﻿//  ___________________________________
+// | Vector.cpp - class implementation |
+// | Jack Flower - June 2019           |
+// |___________________________________|
+//
+
+#include "Vector.h"
+
+namespace scinfor
+{
+	//Konstruktor domyślny
+	Vector::Vector()
+	{
+	}
+
+	//Destruktor wirtualny
+	Vector::~Vector()
+	{
+	}
+}
+//namespace scinfor
---
+++ SCInfor/source/Containers/Vector.h
@@ -0,0 +1,37 @@
+﻿// ____________________________________
+// | Vector.h - definicja klasy        |
+// | Jack Flower - June 2019           |
+// |___________________________________|
+//
+
+#ifndef H_VECTOR_JACK
+#define H_VECTOR_JACK
+
+#include <vector>
+#include <algorithm>
+
+namespace scinfor
+{
+	///
+	///Klasa reprezentuje opakowania kontenera STL (std::vector) wraz
+	///z wbudowanymi mechanizmami oraz implementacją wedgług standardy C++17
+	///
+	class Vector
+	{
+	public:
+
+		///
+		///Konstruktor domyślny
+		///
+		Vector();
+
+		///
+		///Destruktor wirtualny
+		///
+		virtual ~Vector();
+
+	private:
+
+	};
+}//namespace scinfor
+#endif//H_VECTOR_JACK
